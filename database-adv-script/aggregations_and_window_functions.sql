681b5c1a014adba491f829fff3c436cf7e803a4d
Musa  Ogunsolu <136078661+Musakalamz@users.noreply.github.com>
2025-10-30 10:13:18 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,25 +1,28 @@
 -- ===========================================
--- 2. AGGREGATIONS & WINDOW FUNCTIONS
+-- 2. AGGREGATIONS AND WINDOW FUNCTIONS
 -- File: aggregations_and_window_functions.sql
+-- Project: ALX Airbnb Database (Advanced Querying)
 -- ===========================================
 
--- Total number of bookings made by each user
+-- Find the total number of bookings made by each user
 SELECT 
     u.user_id,
-    u.name,
+    u.name AS user_name,
     COUNT(b.booking_id) AS total_bookings
 FROM User AS u
 LEFT JOIN Booking AS b
     ON u.user_id = b.user_id
-GROUP BY u.user_id, u.name;
+GROUP BY u.user_id, u.name
+ORDER BY total_bookings DESC;
 
--- Rank properties based on the number of bookings
+-- Rank properties based on the total number of bookings received
 SELECT 
     p.property_id,
     p.property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank_position
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
 FROM Property AS p
 LEFT JOIN Booking AS b
     ON p.property_id = b.property_id
-GROUP BY p.property_id, p.property_name;
+GROUP BY p.property_id, p.property_name
+ORDER BY property_rank;
